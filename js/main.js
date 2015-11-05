868ec186bfac7878e032bfcaf792f6388867dab5
sundarcodes <sundar82@gmail.com>
2015-11-05 22:48:44 +0530

    Javascript added for handling drag and drop events

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
-  $('.holder').append('<div class="marble"></div>');
   $('.marble').draggable({
     containment: '.playArea',
     cursor: 'move',
-    snap: '.playArea',
-    revert: "invalid"
+    revert: "invalid",
+    stack: '.marble',
+    start: handleDragStart
   });
-  $('.holder').droppable();
-  $('.holder-empty').droppable();
+  function handleDragStart(ev,ui) {
+    // This function will be triggered when the drag starts.
+    // This will be used to highlight the possible moves that could be done
+    console.log("Element drag started");
+  }
+  $('.holder').droppable({
+    drop: handleDropEvent
+    });
+  function handleDropEvent(ev,ui) {
+    console.log("Element dropped");
+  }
 });
