14f449d287b3c12bdf7ba7f296f8953e52230cf9
sundarcodes <sundar82@gmail.com>
2015-11-06 09:40:21 +0530

    Encapsulated brainvita into Javascript object

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-$(document).ready(function(){
-  $('.marble').draggable({
-    containment: '.playArea',
-    cursor: 'move',
-    revert: "invalid",
-    stack: '.marble',
-    start: handleDragStart
+(function(){
+  var BrainVita = {
+    init: function(){
+      $('.marble').draggable({
+        containment: '.playArea',
+        cursor: 'move',
+        revert: "invalid",
+        stack: '.marble',
+        start: BrainVita.handleDragStart
+      });
+
+      $('.holder').droppable({
+        drop: BrainVita.handleDropEvent
+        });
+
+    },
+    handleDragStart: function handleDragStart(ev,ui) {
+      // This function will be triggered when the drag starts.
+      // This will be used to highlight the possible moves that could be done
+      console.log("Element drag started");
+    },
+    handleDropEvent: function handleDropEvent(ev,ui) {
+      console.log("Element dropped");
+    }
+  }
+
+  $(document).ready(function(){
+    BrainVita.init()
   });
-  function handleDragStart(ev,ui) {
-    // This function will be triggered when the drag starts.
-    // This will be used to highlight the possible moves that could be done
-    console.log("Element drag started");
-  }
-  $('.holder').droppable({
-    drop: handleDropEvent
-    });
-  function handleDropEvent(ev,ui) {
-    console.log("Element dropped");
-  }
-});
+})();
